d6c6cb909b1cb78521f36d2abb1aeee283536c5e
Siyu Zhou <siyu.zhou@outlook.com>
2019-08-01 11:09:34 -0700

    Typo

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ for boid in "${boids[@]}"; do
     python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=70 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/${boid}_0 --boid=$boid --instances=1000 --prefix=valid
 done
 
-"Generating evaluation data for boids=20"
+echo "Generating evaluation data for boids=20"
 python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=70 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/20_0 --boid=$boid --instances=1000 --prefix=valid
 
 cd ..

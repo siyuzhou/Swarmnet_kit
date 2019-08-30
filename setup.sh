90598787c79937b0dabf98e3f03e3334b24b2b0c
Siyu Zhou <siyu.zhou@outlook.com>
2019-08-30 13:32:42 -0700

    Reduce trajectory length to 60

--- setup.sh
+++ setup.sh
@@ -14,12 +14,15 @@ batch_size=1000
 
 for boid in "${boids[@]}"; do
     echo "Generating training data for boids=$boid"
-    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=70 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/${boid}_0 --boid=$boid --instances=50000 --prefix=train
+    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/${boid}_0 --boid=$boid --instances=50000 --prefix=train
     echo "Generating evaluation data for boids=$boid"
-    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=70 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/${boid}_0 --boid=$boid --instances=1000 --prefix=valid
+    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/${boid}_0 --boid=$boid --instances=1000 --prefix=valid
 done
 
 echo "Generating evaluation data for boids=20"
-python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=70 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/20_0 --boid=$boid --instances=1000 --prefix=valid
+python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/20_0 --boid=20 --instances=1000 --prefix=valid
+
+echo "Generating evaluation data for boids=50"
+python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --vision=10 --save-dir=../data/boid/vision_10/50_0 --boid=50 --instances=1000 --prefix=valid
 
 cd ..

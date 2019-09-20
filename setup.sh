91c960f2cb0f37a0acdfa08068086292bd5bff8f
Siyu Zhou <siyu.zhou@outlook.com>
2019-09-20 16:32:10 -0700

    Add vicsek data

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ mkdir -p data
 cd swarms
 git pull
 
-train_boids=( 5 10 )
+#### Boid data
+train_boids=( 5 10 20 )
 processes=20
 batch_size=1000
 
@@ -30,10 +31,39 @@ for file in ../data/boid/10_0/*.npy; do
 done
 
 # Validation sets.
-valid_boids=( 3 5 7 10 20 )
+valid_boids=( 3 5 7 10 20 40)
 for boid in "${valid_boids[@]}"; do
     echo "Generating validation data for boids=$boid"
-    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --save-dir=../data/boid/${boid}_0 --boid=$boid --instances=1000 --prefix=valid
+    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --save-dir=../data/boid/${boid}_0 --boids=$boid --instances=1000 --prefix=valid
+done
+
+
+#### Vicsek data
+train_vicsek=( 5 10 20 )
+processes=20
+batch_size=1000
+
+for vicsek in "${train_vicsek[@]}"; do
+    echo "Generating training data for vicsek=$vicsek"
+    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --save-dir=../data/vicsek/${vicsek}_0 --vicseks=$vicsek --instances=50000 --prefix=train
+done
+
+# Copy 5_0 and 10_0 files to make a mixed set.
+mkdir -p ../data/vicsek/5_10_0
+
+for file in ../data/vicsek/5_0/*.npy; do
+    cp $file ../data/vicsek/5_10_0/$(basename $file .npy)_5.npy
+done
+
+for file in ../data/vicsek/10_0/*.npy; do
+    cp $file ../data/vicsek/5_10_0/$(basename $file .npy)_10.npy
+done
+
+# Validation sets.
+valid_vicseks=( 3 5 7 10 20 40)
+for vicsek in "${valid_vicseks[@]}"; do
+    echo "Generating validation data for vicseks=$vicsek"
+    python3 obstacle_avoidance_sim.py --processes=$processes --batch-size=$batch_size --obstacles=0 --steps=60 --dt=0.3 --save-dir=../data/vicsek/${vicsek}_0 --vicseks=$vicsek --instances=1000 --prefix=valid
 done
 
 cd ..

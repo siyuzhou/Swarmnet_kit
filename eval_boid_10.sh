0a971826f2082fa84002265b03981741fb58ab4b
Siyu Zhou <siyu.zhou@outlook.com>
2019-09-22 17:31:32 -0700

    Optimize training efficiency.

---
+++ eval_boid_10.sh
@@ -0,0 +1,19 @@
+cd swarmnet
+
+BATCH_SIZE=512
+ID=${1:-""}
+
+CONFIG=../config_edge.json 
+LOG_DIR=../logs/boid/10_0/edge_skip$ID
+DATA_DIR=../data/boid/
+
+
+DATA_SETS=( "3_0" "5_0" "7_0" "10_0" "20_0" "40_0") 
+
+for ((i=0;i<${#DATA_SETS[@]};i++)); do
+    data_set=${DATA_SETS[$i]}
+    echo "Eval loss for $data_set" >> $LOG_DIR/eval.txt
+    python3 run_swarmnet.py --eval --data-dir=$DATA_DIR/$data_set --config=$CONFIG --log-dir=$LOG_DIR  --pred-steps=40 --batch-size=$BATCH_SIZE >> $LOG_DIR/eval.txt
+done
+
+cat $LOG_DIR/eval.txt
--- train_boid_pad_tune.sh
+++ train_boid_pad_tune.sh
@@ -2,11 +2,12 @@
 cd swarmnet
 git pull
 
-BATCH_SIZE=1024
 ID=${1:-""}
 
+BATCH_SIZE=2048
+
 CONFIG=../config_edge.json 
-LOG_DIR=../logs/boid/5_10_tune/edge_skip$ID
+LOG_DIR=../logs/boid/5_0/edge_skip$ID
 DATA_DIR=../data/boid/
 
 mkdir -p $LOG_DIR
@@ -16,16 +17,21 @@ PRED_STEPS=( 1 3 5 10 )
 EPOCHS=( 10 5 3 3 )
 BATCH_SIZES=( $BATCH_SIZE $(($BATCH_SIZE / 2)) $(($BATCH_SIZE / 4)) $(($BATCH_SIZE / 8)) )
 DATA_SET=5_0
-# PADDING=11
+
 
 for ((i=0;i<${#PRED_STEPS[@]};i++)); do
-    python3 run_swarmnet.py --train --data-dir=$DATA_DIR/$DATA_SET --config=$CONFIG --log-dir=$LOG_DIR --epochs=${EPOCHS[$i]} --pred-steps=${PRED_STEPS[$i]} --batch-size=${BATCH_SIZES[$i]} #--max-padding=$PADDING
+    python3 run_swarmnet.py --train --data-dir=$DATA_DIR/$DATA_SET --config=$CONFIG --log-dir=$LOG_DIR --epochs=${EPOCHS[$i]} --pred-steps=${PRED_STEPS[$i]} --batch-size=${BATCH_SIZES[$i]}
     echo "Training with $DATA_SET and ${PRED_STEPS[$i]} pred_steps over ${EPOCHS[$i]} epochs done" >> $LOG_DIR/train_log.txt
 done
 
 
+BATCH_SIZE=1024
+
+TUNE_LOG_DIR=../logs/boid/5_10_tune/edge_skip$ID
+cp -r $LOG_DIR $TUNE_LOG_DIR
+
 PRED_STEPS=( 1 3 5 10 )
-EPOCHS=( 5 3 2 1 )
+EPOCHS=( 5 3 2 2 )
 BATCH_SIZES=( $BATCH_SIZE $(($BATCH_SIZE / 2)) $(($BATCH_SIZE / 4)) $(($BATCH_SIZE / 8)) )
 DATA_SET=5_10_0
 PADDING=11

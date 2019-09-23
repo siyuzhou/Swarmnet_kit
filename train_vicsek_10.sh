a232d60f90078b72454696f4a2f184a2caf028c7
Siyu Zhou <siyu.zhou@outlook.com>
2019-09-23 08:32:41 -0700

    Add vicsek_10

---
+++ train_vicsek_10.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd swarmnet
+git pull
+
+BATCH_SIZE=1024
+ID=${1:-""}
+
+CONFIG=../config_edge.json 
+LOG_DIR=../logs/vicsek/10_0/edge_skip$ID
+DATA_DIR=../data/vicsek/
+
+mkdir -p $LOG_DIR
+cp $CONFIG $LOG_DIR/
+
+PRED_STEPS=( 1 3 5 10 )
+EPOCHS=( 10 5 3 3 )
+BATCH_SIZES=( $BATCH_SIZE $(($BATCH_SIZE / 2)) $(($BATCH_SIZE / 4)) $(($BATCH_SIZE / 8)) )
+DATA_SET=10_0
+# PADDING=11
+
+for ((i=0;i<${#PRED_STEPS[@]};i++)); do
+    python3 run_swarmnet.py --train --data-dir=$DATA_DIR/$DATA_SET --config=$CONFIG --log-dir=$LOG_DIR --epochs=${EPOCHS[$i]} --pred-steps=${PRED_STEPS[$i]} --batch-size=${BATCH_SIZES[$i]} #--max-padding=$PADDING
+    echo "Training with $DATA_SET and ${PRED_STEPS[$i]} pred_steps over ${EPOCHS[$i]} epochs done" >> $LOG_DIR/train_log.txt
+done
+
+
+echo "Complete!" >> $LOG_DIR/train_log.txt
+cd ..
